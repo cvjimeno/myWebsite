7dd7a3332c2eb1349c7c469dc580e90e986af210
Carlos Velasco <cvjimeno@gmail.com>
2025-07-02 14:31:29 +0200

    icon refactor. use lucide for normal icons, and my own  downloaded svgs for socials

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
-
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mywebsitecvcv.netlify.app', 
-
+  site: 'https://mywebsitecvcv.netlify.app',
+  
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [
+    mdx(), 
+    sitemap(), 
+    // We only need the Lucide pack now. The custom collection is gone.
+    icon({
+      include: {
+        'lucide': ['*'],
+      }
+    })
+  ],
 });
